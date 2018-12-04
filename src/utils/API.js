5ad87b5aa0ae6a4aa2e6e7648ac589bd2fa41cd8
Noah Neighbors <noah@gangplankhq.com>
2018-12-04 15:28:28 -0700

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 // api calls
 export default {
     renderBoard: function(id){
-        return axios.get(`https://optihqintern:YtXa8b2m5NFNpfNC@intern-dev.opthq.net/api/searchSampleData?surveyId=${id}`)
+        return axios.get(`https://intern-dev.opthq.net/api/searchSampleData?surveyId=${id}`)
     },
 }
